2d3645813a467969599ca277bff1dd4f9bf2f89a
David <David@DESKTOP-SHAL13A>
2021-07-15 16:20:42 -0500

    botones de cerrar y minimizar agregaos

--- ProyectoIntegrador/Form1.Designer.cs
+++ ProyectoIntegrador/Form1.Designer.cs
@@ -46,9 +46,11 @@ namespace ProyectoIntegrador
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 330);
+            this.panel1.Size = new System.Drawing.Size(375, 508);
             this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
             // txt_usu
             // 
@@ -56,10 +58,11 @@ namespace ProyectoIntegrador
             this.txt_usu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txt_usu.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_usu.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.txt_usu.Location = new System.Drawing.Point(340, 124);
+            this.txt_usu.Location = new System.Drawing.Point(510, 191);
+            this.txt_usu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_usu.Name = "txt_usu";
-            this.txt_usu.Size = new System.Drawing.Size(289, 22);
-            this.txt_usu.TabIndex = 1;
+            this.txt_usu.Size = new System.Drawing.Size(432, 29);
+            this.txt_usu.TabIndex = 3;
             this.txt_usu.Text = "Usuario";
             this.txt_usu.Enter += new System.EventHandler(this.txt_usu_Enter);
             this.txt_usu.Leave += new System.EventHandler(this.txt_usu_Leave);
@@ -68,9 +71,10 @@ namespace ProyectoIntegrador
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(427, 62);
+            this.label1.Location = new System.Drawing.Point(640, 95);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.Size = new System.Drawing.Size(130, 39);
             this.label1.TabIndex = 3;
             this.label1.Text = "LOGIN";
             // 
@@ -80,10 +84,11 @@ namespace ProyectoIntegrador
             this.txt_contra.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txt_contra.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_contra.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.txt_contra.Location = new System.Drawing.Point(340, 180);
+            this.txt_contra.Location = new System.Drawing.Point(510, 277);
+            this.txt_contra.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_contra.Name = "txt_contra";
-            this.txt_contra.Size = new System.Drawing.Size(289, 22);
-            this.txt_contra.TabIndex = 4;
+            this.txt_contra.Size = new System.Drawing.Size(432, 29);
+            this.txt_contra.TabIndex = 2;
             this.txt_contra.Text = "CONTRASEÑA";
             this.txt_contra.Enter += new System.EventHandler(this.txt_contra_Enter);
             this.txt_contra.Leave += new System.EventHandler(this.txt_contra_Leave);
@@ -96,9 +101,10 @@ namespace ProyectoIntegrador
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(415, 238);
+            this.button1.Location = new System.Drawing.Point(622, 366);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 34);
+            this.button1.Size = new System.Drawing.Size(177, 52);
             this.button1.TabIndex = 5;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = false;
@@ -106,29 +112,33 @@ namespace ProyectoIntegrador
             // btn_cerrar
             // 
             this.btn_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrar.Image")));
-            this.btn_cerrar.Location = new System.Drawing.Point(693, 12);
+            this.btn_cerrar.Location = new System.Drawing.Point(1044, 14);
+            this.btn_cerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_cerrar.Name = "btn_cerrar";
-            this.btn_cerrar.Size = new System.Drawing.Size(16, 22);
-            this.btn_cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_cerrar.Size = new System.Drawing.Size(38, 34);
+            this.btn_cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btn_cerrar.TabIndex = 6;
             this.btn_cerrar.TabStop = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
             // 
             // btn_mini
             // 
             this.btn_mini.Image = ((System.Drawing.Image)(resources.GetObject("btn_mini.Image")));
-            this.btn_mini.Location = new System.Drawing.Point(662, 12);
+            this.btn_mini.Location = new System.Drawing.Point(998, 14);
+            this.btn_mini.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_mini.Name = "btn_mini";
-            this.btn_mini.Size = new System.Drawing.Size(25, 22);
+            this.btn_mini.Size = new System.Drawing.Size(38, 34);
             this.btn_mini.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.btn_mini.TabIndex = 7;
             this.btn_mini.TabStop = false;
+            this.btn_mini.Click += new System.EventHandler(this.btn_mini_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(94)))), ((int)(((byte)(162)))));
-            this.ClientSize = new System.Drawing.Size(730, 330);
+            this.ClientSize = new System.Drawing.Size(1095, 508);
             this.Controls.Add(this.btn_mini);
             this.Controls.Add(this.btn_cerrar);
             this.Controls.Add(this.button1);
@@ -137,11 +147,11 @@ namespace ProyectoIntegrador
             this.Controls.Add(this.txt_usu);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             ((System.ComponentModel.ISupportInitialize)(this.btn_cerrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btn_mini)).EndInit();
             this.ResumeLayout(false);
--- ProyectoIntegrador/Form1.cs
+++ ProyectoIntegrador/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//libreria para mover el formulario
+using System.Runtime.InteropServices;
 
 namespace ProyectoIntegrador
 {
@@ -16,6 +18,10 @@ namespace ProyectoIntegrador
         {
             InitializeComponent();
         }
+        [DllImport("user32.Dll", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.Dll", EntryPoint = "sendMessage")]
+        private extern static void sendMessage(System.IntPtr hand, int wmsg, int wparam, int lparam);
 
         private void txt_usu_Enter(object sender, EventArgs e)
         {
@@ -58,6 +64,28 @@ namespace ProyectoIntegrador
                 txt_contra.UseSystemPasswordChar = false;    
 
             }
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_mini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            sendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            sendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
